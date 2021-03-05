d4d6f55add6d1130c589abef4c688d99fd7aff97
Jason Pinlac <jasonpinlacdev@gmail.com>
2021-03-05 11:06:50 -0500

    Learning concurrency using Operation and OperationQueue

--- ConcurrencyTest/ConcurrencyTest/Controllers/ViewController.swift
+++ ConcurrencyTest/ConcurrencyTest/Controllers/ViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var primeNumberButton: UIButton!
     
@@ -16,25 +16,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func calculatePrimeNumbers(_ sender: UIButton) {
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            (1...1_000_000).forEach { print("\($0): \(self.isPrime(number: $0))") }
-        }
-      
+        let queue = OperationQueue()
+        let operation = CalculatePrimeOperation()
+        queue.addOperation(operation)
+       
     }
     
-    func isPrime(number: Int) -> Bool {
-        if number <= 1 { return false }
-        if number <= 3 { return true }
-        var i = 2
-        while i < number {
-            if number % i == 0 { return false }
-            i += 1
-        }
-        return true
-    }
+   
     
 }
 
