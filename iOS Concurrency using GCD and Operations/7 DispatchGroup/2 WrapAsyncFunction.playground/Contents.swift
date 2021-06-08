94fe12ae4758311252301558ccb8a9dcbc6b2029
Jason Pinlac <jasonpinlacdev@gmail.com>
2021-06-08 13:05:12 -0400

    Finished DispatchGroups, its use cases, and real examples

---
+++ iOS Concurrency using GCD and Operations/7 DispatchGroup/2 WrapAsyncFunction.playground/Contents.swift
@@ -0,0 +1,66 @@
+// Copyright (c) 2020 Razeware LLC
+// For full license & permission details, see LICENSE.markdown.
+
+import UIKit
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+//: # Wrapping an Asynchronous Function for DispatchGroup
+//: There are lots of asynchronous APIs that don't have group parameters. What can you do with them, so the group knows when they *really* finish?
+//:
+//: Remember from Part 1 the `slowAdd` function you wrapped as an asynchronous function?
+let numberArray = [(0,1), (2,3), (4,5), (6,7), (8,9)]
+
+func asyncAdd(_ input: (Int, Int),
+  runQueue: DispatchQueue = DispatchQueue.global(qos: .userInitiated),
+  completionQueue: DispatchQueue = DispatchQueue.main,
+  completionHandler: @escaping (Result<Int, SlowAddError>) -> ()) {
+
+  runQueue.async {
+    let result = slowAddPlus(input)
+    completionQueue.async { completionHandler(result) }
+  }
+  
+}
+//: Wrap `asyncAdd` function
+// TODO: Create asyncAdd_Group
+func asyncAdd_Group(_ input: (Int, Int),
+                    runQueue: DispatchQueue = DispatchQueue.global(qos: .userInitiated),
+                    completionQueue: DispatchQueue = DispatchQueue.main,
+                    group: DispatchGroup,
+                    completionHandler: @escaping (Result<Int, SlowAddError>) -> ()) {
+  
+  group.enter()
+  asyncAdd(input) { result in
+    defer { group.leave() }
+    completionQueue.async { completionHandler(result) }
+  }
+
+}
+
+
+
+
+
+print("\n=== Group of async tasks ===\n")
+let wrappedGroup = DispatchGroup()
+
+for pair in numberArray {
+  // TODO: use the new function here to calculate the sums of the array
+  asyncAdd_Group(pair, group: wrappedGroup) { result in
+    switch result {
+    case .success(let number):
+      print("\(pair): \(number)")
+    case .failure(let error):
+      print("\(pair): \(error)")
+    }
+  }
+}
+
+// TODO: Notify of completion
+
+wrappedGroup.notify(queue: DispatchQueue.main) {
+  print("All tasks in the wrapped group have completed!")
+  PlaygroundPage.current.finishExecution()
+}
+
+
